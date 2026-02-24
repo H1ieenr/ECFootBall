8fe307f05c4c0ca2188106751961c1e7638173d5
DESKTOP-I1S5SR8\admin <letanhien.it@gmail.com>
2026-02-24 12:07:18 +0700

    feat: complete Cloudinary service in Infrastructure.Shared

---
+++ ECFootball.Infrastructure.Shared/_Services/Interfaces/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+
+namespace ECFootball.Infrastructure.Shared._Services.Interfaces
+{
+    public interface IFileService
+    {
+        Task<ImageUploadResult> UploadImageAsync(IFormFile file, string folderName);
+        Task<DeletionResult> DeleteImageAsync(string publicId);
+    }
+}
---
+++ ECFootball.Infrastructure.Shared/_Services/Services/FileService.cs
@@ -0,0 +1,48 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using ECFootball.Infrastructure.Shared._Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace ECFootball.Infrastructure.Shared._Services.Services
+{
+    public class FileService : IFileService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public FileService(IConfiguration config)
+        {
+            // Lấy thông tin từ appsettings.json của project API
+            var acc = new Account(
+                config["CloudinarySettings:CloudName"],
+                config["CloudinarySettings:ApiKey"],
+                config["CloudinarySettings:ApiSecret"]
+            );
+            _cloudinary = new Cloudinary(acc);
+        }
+
+        public async Task<ImageUploadResult> UploadImageAsync(IFormFile file, string folderName)
+        {
+            var uploadResult = new ImageUploadResult();
+            if (file.Length > 0)
+            {
+                using var stream = file.OpenReadStream();
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Folder = folderName,
+                    Transformation = new Transformation().Quality("auto").FetchFormat("auto")
+                };
+                uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            }
+            return uploadResult;
+        }
+
+        public async Task<DeletionResult> DeleteImageAsync(string publicId)
+        {
+            var deleteParams = new DeletionParams(publicId);
+            return await _cloudinary.DestroyAsync(deleteParams);
+        }
+    
+    }
+}
